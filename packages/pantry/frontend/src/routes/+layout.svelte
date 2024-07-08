401dea085ceaf5c5799cfdb8b2173df02a036d58
Ben Allfree <ben@benallfree.com>
2024-07-08 16:22:30 -0700

    wip

--- packages/pantry/frontend/src/routes/+layout.svelte
+++ packages/pantry/frontend/src/routes/+layout.svelte
@@ -1,37 +1,16 @@
-<script lang="ts">
-  import MediaQuery from '$components/MediaQuery.svelte'
-  import MobileNavDrawer from '$components/MobileNavDrawer.svelte'
-  import Navbar from '$components/Navbar.svelte'
-  import VerifyAccountBar from '$components/VerifyAccountBar.svelte'
-  import AuthStateGuard from '$components/helpers/AuthStateGuard.svelte'
-  import Meta from '$components/helpers/Meta.svelte'
-  import UserLoggedIn from '$components/helpers/UserLoggedIn.svelte'
-  import { isUserLoggedIn } from '$util/stores'
-  import '../app.css'
+<script>
+	import Header from './Header.svelte';
+	import '../app.css';
 </script>
 
-<Meta />
+<div class="app">
+	<Header />
 
-<AuthStateGuard>
-  <div>
-    <UserLoggedIn>
-      <MediaQuery query="(max-width: 700px)" let:matches>
-        {#if matches}
-          <MobileNavDrawer>
-            <Navbar />
-          </MobileNavDrawer>
-        {:else}
-          <Navbar />
-        {/if}
-      </MediaQuery>
-    </UserLoggedIn>
+	<main>
+		<slot />
+	</main>
 
-    <main class="py-10 {$isUserLoggedIn ? `lg:pl-72` : ``}">
-      <div class="px-4 sm:px-6 lg:px-8">
-        <VerifyAccountBar />
-
-        <slot />
-      </div>
-    </main>
-  </div>
-</AuthStateGuard>
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>
--- packages/pantry/frontend/svelte.config.js
+++ packages/pantry/frontend/svelte.config.js
@@ -1,23 +1,18 @@
-import adapter from '@sveltejs/adapter-static'
-import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess(),
-  kit: {
-    adapter: adapter({
-      pages: '.dist',
-      fallback: 'index.html',
-    }),
-    alias: {
-      $components: './src/components',
-      $util: './src/util',
-      $src: './src',
-    },
-  },
-  target: '#svelte',
-}
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 
-export default config
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter({})
+	}
+};
+
+export default config;
