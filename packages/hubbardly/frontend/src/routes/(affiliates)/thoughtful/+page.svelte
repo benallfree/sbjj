8434dcaf02cfa534fc71cc7062ea2b22c4eda925
Ben Allfree <ben@benallfree.com>
2024-07-14 12:03:25 -0700

    hubbardly: thoughtful demo - hoist names

--- packages/hubbardly/frontend/src/routes/(affiliates)/thoughtful/+page.svelte
+++ packages/hubbardly/frontend/src/routes/(affiliates)/thoughtful/+page.svelte
@@ -6,13 +6,14 @@
   import { produce } from 'immer'
   import PricingSheet from '$src/routes/(marketing)/pricing/PricingSheet.svelte'
   import FaqSection from '$src/components/FAQSection.svelte'
+  import { APP_NAME, FOUNDERS_NAME } from '$src/meta/constants'
 
   const merged = produce(meta, (draft) => {
     const { hero } = draft.pages.lander
     hero.title = `Welcome, Thoughtful Journey listeners`
     hero.splashImg = splashImg
-    hero.cta.text = `We've partnered with Hubbardly to create a special Founder's Edition just for TJ listeners. Purchase a #Hubbardly Founder's Edition: TJ Special# to  get a #lifetime membership# to Hubbardly with #TJ-exclusive bonuses#. This is rare, don't sleep on it!`
-    hero.cta.button.text = `Get Hubbardly Founder's Edition, TJ Special`
+    hero.cta.text = `We've partnered with ${APP_NAME} to create a special ${FOUNDERS_NAME} just for TJ listeners. Purchase a #${APP_NAME} ${FOUNDERS_NAME} TJ Special# to  get a #lifetime membership# to ${APP_NAME} with #TJ-exclusive bonuses#. This is rare, don't sleep on it!`
+    hero.cta.button.text = `Get ${APP_NAME} ${FOUNDERS_NAME}: TJ Special`
     hero.cta.button.link = 'https://buy.stripe.com/fZe28s0wrgLVeuA5kl'
     draft.plans.founder.name = `Founder's Edition - TJ Special`
     draft.plans.founder.checkoutUrl =
