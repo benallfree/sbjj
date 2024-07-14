b7098c29eb527126dca23b0751db566d2b089e28
Ben Allfree <ben@benallfree.com>
2024-07-14 09:19:02 -0700

    hubberdly: format number cleanup

--- packages/hubbardly/frontend/src/routes/(marketing)/pricing/+page.svelte
+++ packages/hubbardly/frontend/src/routes/(marketing)/pricing/+page.svelte
@@ -8,21 +8,9 @@
   const {
     prelaunch,
     pages: {
-      pricing: { title, tagline, content },
+      pricing: { title, tagline },
     },
   } = meta
-
-  function formatNumber(num: number): string {
-    if (num < 1000) {
-      return num.toString()
-    } else if (num < 1_000_000) {
-      return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'k'
-    } else if (num < 1_000_000_000) {
-      return (num / 1_000_000).toFixed(1).replace(/\.0$/, '') + 'm'
-    } else {
-      return (num / 1_000_000_000).toFixed(1).replace(/\.0$/, '') + 'b'
-    }
-  }
 </script>
 
 <main>
--- packages/hubbardly/frontend/src/routes/(marketing)/pricing/PricingSheet.svelte
+++ packages/hubbardly/frontend/src/routes/(marketing)/pricing/PricingSheet.svelte
@@ -7,21 +7,9 @@
   import ListItem from './ListItem.svelte'
   import PricingTier from './PricingTier.svelte'
   import type { Meta } from '$src/meta'
-  import { greenify } from '$src/util'
+  import { formatNumber, greenify } from '$src/util'
 
   export let meta: Meta
-
-  function formatNumber(num: number): string {
-    if (num < 1000) {
-      return num.toString()
-    } else if (num < 1_000_000) {
-      return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'k'
-    } else if (num < 1_000_000_000) {
-      return (num / 1_000_000).toFixed(1).replace(/\.0$/, '') + 'm'
-    } else {
-      return (num / 1_000_000_000).toFixed(1).replace(/\.0$/, '') + 'b'
-    }
-  }
 </script>
 
 <div class="flex flex-col lg:flex-row gap-x-2">
