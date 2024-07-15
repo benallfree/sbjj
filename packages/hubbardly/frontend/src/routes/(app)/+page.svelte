a87def9dda64099484d792ff15a257f3eefc47c0
Ben Allfree <ben@benallfree.com>
2024-07-15 02:58:37 -0700

    hubbardly: logged in landing page

---
+++ packages/hubbardly/frontend/src/routes/(app)/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import UserLoggedIn from '$src/components/UserLoggedIn.svelte'
+  import UserLoggedOut from '$src/components/UserLoggedOut.svelte'
+  import LoggedOutLander from './LoggedOutLander.svelte'
+  import Empty from './Empty.svx'
+</script>
+
+<UserLoggedIn>
+  <article class="prose lg:prose-xl m-4">
+    <Empty />
+  </article>
+</UserLoggedIn>
+<UserLoggedOut>
+  <LoggedOutLander />
+</UserLoggedOut>
